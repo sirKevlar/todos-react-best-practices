6668a9c54d712aabba469e75a4f56153951f7448
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-11-28 22:44:40 +0000

    alter todo item prop types

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import { TodoItemProps } from "../lib/types/types";
 import DeleteButton from "./DeleteButton";
 
-export default function TodoItem({todo: {id, text, priority, finishBy}}: TodoItemProps) {
+export default function TodoItem({id, text, priority, finishBy}: TodoItemProps) {
     const finishDate = `${finishBy[4]}${finishBy[5]}/${finishBy[2]}${finishBy[3]}/20${finishBy[0]}${finishBy[1]}`
     return (
     <div className="todo flex justify-between"><p>{priority}</p><p>{text} by {finishDate} </p><DeleteButton id={id} onClick={()=>{}}/></div>
