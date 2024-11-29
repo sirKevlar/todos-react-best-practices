7155598983d06937a78624d7767e3c9a501621bd
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-11-29 18:50:24 +0000

    update constants

---
+++ src/lib/constants/constants.ts
@@ -0,0 +1,3 @@
+export const INITIAL_TODOS = [{id:'1', text: "do this thing right away", priority: "🔴", finishBy: "241215"},{id:'2', text: "do this thing soon", priority: "🟡", finishBy: "241215"},{id:'3', text: "do this thing in a bit", priority: "🟢", finishBy: "241225"}]
+
+export const GUEST_USER = {firstName: "Guest", lastName: "User", email: "example@email.com", id: "1", userLevel: "guest"}
