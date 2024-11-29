be859a4d7644539ae13d78f399381cbb8bd1c712
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-11-29 18:50:39 +0000

    update types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,3 +1,9 @@
+import React, { FormEventHandler, MouseEventHandler } from "react"
+
+
+export type AddTodoFormProps = {
+    addTodo: (newTodo: TodoItemProps) => void
+}
 export type AdvertProps = {
     id: string
     imgUrl: string 
@@ -6,7 +12,9 @@ export type AdvertProps = {
 export type ButtonProps = {
     id: string
     children: string
-    onClick: () => void
+    type: "submit" | "reset" | "button"
+    onClick?: MouseEventHandler<HTMLButtonElement>
+    onSubmit?: FormEventHandler<HTMLButtonElement>
 }
 export type ContainerProps = {
     id: string
@@ -23,15 +31,32 @@ export type DeleteProps = {
 export type InputProps = {
     type: "text" | "radio" | "checkbox"
     id: string 
+    name: string
+    placeholder: string
     val: string | number 
-    setVal: (value: string | number) => void
+    handleChange: (value: string, name: string) => void
+}
+export type ProgressBarProps = {
+    progressPercentage: string
 }
 export type TodoItemProps = { 
+    children?: React.ReactNode
     id: string 
     text: string 
     priority: string 
     finishBy: string 
 }
 export type TodoListProps = {
-    todos: TodoItemProps[]
+    todoList: TodoItemProps[]
+    removeTodo: (id: string) => void
 }
+export type UserProviderProps = {
+    children: React.ReactNode
+    setUser: React.SyntheticEvent
+}
+export type User = {
+    firstName: string 
+    lastName: string 
+    email: string 
+    id: string 
+    userLevel: "guest" | "admin"}
