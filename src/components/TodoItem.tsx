87131e8d0999de35b87064dc17e6137db82bf7cc
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-11-29 18:46:42 +0000

    add children to props

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,8 @@
 import { TodoItemProps } from "../lib/types/types";
-import DeleteButton from "./DeleteButton";
 
-export default function TodoItem({id, text, priority, finishBy}: TodoItemProps) {
+export default function TodoItem({children, id, text, priority, finishBy}: TodoItemProps) {
     const finishDate = `${finishBy[4]}${finishBy[5]}/${finishBy[2]}${finishBy[3]}/20${finishBy[0]}${finishBy[1]}`
     return (
-    <div className="todo flex justify-between"><p>{priority}</p><p>{text} by {finishDate} </p><DeleteButton id={id} onClick={()=>{}}/></div>
+    <div id={id} className="todo flex justify-between"><p>{priority}</p><p>{text} by {finishDate} </p>{children}</div>
   )
 }
