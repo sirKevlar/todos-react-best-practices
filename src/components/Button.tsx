3b21e4fc5e9570d70240531d1006587fb89e1ac1
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-11-29 18:45:52 +0000

    update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ButtonProps } from "../lib/types/types";
 
-export default function Button({id, children, onClick}: ButtonProps) {
+export default function Button({id, children, type, onClick, onSubmit}: ButtonProps) {
   return (
-    <button id={id} onClick={onClick}>{children}</button>
+    <button type={type} id={id} onClick={onClick} onSubmit={onSubmit}>{children}</button>
   )
 }
