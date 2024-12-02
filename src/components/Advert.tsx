ea9588e85df5dfe2018f29a7ad34c7514d6f6111
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-02 07:49:58 +0000

    add styling class to advert

--- src/components/Advert.tsx
+++ src/components/Advert.tsx
@@ -2,7 +2,7 @@ import { AdvertProps } from "../lib/types/types";
 
 export default function Advert({id, image, link}: AdvertProps) {
   return (
-    <a href={link}>
+    <a href={link} className="advert-link">
         <img className="advert" id={id} src={image}/>
     </a>
   )
