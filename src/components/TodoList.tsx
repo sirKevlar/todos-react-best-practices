194acf721acf43b6cf4d88ed4c81b44eb891ca31
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-02 07:49:11 +0000

    add status to list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,15 +2,15 @@ import { TodoItemProps, TodoListProps } from "../lib/types/types"
 import DeleteButton from "./DeleteButton"
 import TodoItem from "./TodoItem"
 
-export default function TodoList({todoList, removeTodo}: TodoListProps) {
+export default function TodoList({todoList, removeTodo, changeTodoStatus}: TodoListProps) {
 
   return (
-    <ul>
-        {todoList.map(({id, text, priority, finishBy}: TodoItemProps)=>{
+    <ul id="todo-list">
+        {todoList.map(({id, text, priority, finishBy, status}: TodoItemProps)=>{
             const key = `todoItem${id}`
 
             return (
-              <TodoItem key={key} id={id} text={text} priority={priority} finishBy={finishBy}>
+              <TodoItem changeTodoStatus={changeTodoStatus} key={key} id={id} status={status} text={text} priority={priority} finishBy={finishBy}>
                 <DeleteButton id={id} onClick={()=>{removeTodo(id)}}/>
               </TodoItem>
               )
