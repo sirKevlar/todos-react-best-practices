696de05ffe28abebed70e6fa80a14aa65f63b3de
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-02 19:37:12 +0000

    add functions to delete props

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,7 +1,7 @@
 import { DeleteProps } from "../lib/types/types";
 
-export default function DeleteButton({id, onClick}: DeleteProps) {
+export default function DeleteButton({id, removeTodo, setTodoList}: DeleteProps) {
   return (
-    <button id={id} onClick={onClick} className="delete">❌</button>
+    <button id={id} onClick={()=>{removeTodo(id, setTodoList)}} className="delete">❌</button>
   )
 }
