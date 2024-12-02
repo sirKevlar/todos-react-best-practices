fe841fa2955d18f3b3385c6fa6dcd381883be3e4
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-02 19:41:19 +0000

    extract functions

---
+++ src/lib/utils/events.tsx
@@ -0,0 +1,31 @@
+import { TodoItemType } from "../types/types";
+
+
+export const addTodo = (newTodo: TodoItemType, setTodoList: React.Dispatch<React.SetStateAction<TodoItemType[]>>) => {
+    setTodoList((curr)=>{
+      return [...curr, newTodo]
+    })
+  }
+
+export const removeTodo = (id: string, setTodoList: React.Dispatch<React.SetStateAction<TodoItemType[]>> ): void => {
+    setTodoList((curr: TodoItemType[]) => {
+        const newList = curr.filter((todoItem: TodoItemType)=>{
+          return todoItem.id !== id
+        })
+        return newList
+      })
+}
+
+export const changeTodoStatus = (id: string, setTodoList: React.Dispatch<React.SetStateAction<TodoItemType[]>> ) => { console.log('invoked');
+
+    setTodoList((curr: TodoItemType[])=>{
+        console.log('setting-list');
+      const newList = curr.map((todoItem: TodoItemType)=>{
+          if(todoItem.id === id) {
+            todoItem.status = todoItem.status === 'complete' ? 'incomplete' : 'complete'
+        }
+        return todoItem
+      })
+      return newList
+    })
+  }
