18e87c512b2de85b434b129e12f88aec1c891b80
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-02 19:37:44 +0000

    add function to props

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,9 @@
 import { TodoItemProps } from "../lib/types/types";
 
-export default function TodoItem({children, id, text, priority, finishBy, status, changeTodoStatus }: TodoItemProps) {
+export default function TodoItem({children, id, text, priority, finishBy, status, changeTodoStatus, setTodoList }: TodoItemProps) {
     const finishDate = `${finishBy[4]}${finishBy[5]}/${finishBy[2]}${finishBy[3]}/20${finishBy[0]}${finishBy[1]}`
 
     return (
-    <div id={id} className="todo flex justify-between"><p id={`priority${id}`}>{priority}</p><p className={status} onClick={()=>{changeTodoStatus(id)}}>{text} by {finishDate} </p>{children}</div>
+    <div id={id} className="todo flex justify-between"><p id={`priority${id}`}>{priority}</p><p className={`todo-text ${status}`} onClick={()=>{changeTodoStatus(id, setTodoList)}}>{text} by {finishDate} </p>{children}</div>
   )
 }
