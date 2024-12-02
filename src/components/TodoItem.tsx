5d96e3be4b8b7cef204d921515caf93723ab641b
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-02 07:48:55 +0000

    add status to item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,9 @@
 import { TodoItemProps } from "../lib/types/types";
 
-export default function TodoItem({children, id, text, priority, finishBy}: TodoItemProps) {
+export default function TodoItem({children, id, text, priority, finishBy, status, changeTodoStatus }: TodoItemProps) {
     const finishDate = `${finishBy[4]}${finishBy[5]}/${finishBy[2]}${finishBy[3]}/20${finishBy[0]}${finishBy[1]}`
+
     return (
-    <div id={id} className="todo flex justify-between"><p>{priority}</p><p>{text} by {finishDate} </p>{children}</div>
+    <div id={id} className="todo flex justify-between"><p id={`priority${id}`}>{priority}</p><p className={status} onClick={()=>{changeTodoStatus(id)}}>{text} by {finishDate} </p>{children}</div>
   )
 }
