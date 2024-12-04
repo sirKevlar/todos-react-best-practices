4bbbd816f59c6dba03a81bf5c62b625fb97ec3af
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-04 08:16:11 +0000

    add class to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,6 @@ import { ButtonProps } from "../lib/types/types";
 
 export default function Button({id, children, type, onClick, onSubmit}: ButtonProps) {
   return (
-    <button type={type} id={id} onClick={onClick} onSubmit={onSubmit}>{children}</button>
+    <button type={type} id={id} className='primary-button' onClick={onClick} onSubmit={onSubmit}>{children}</button>
   )
 }
