041cc4488c8e08b431eb05e9ea349bbe272bab0c
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-04 08:17:56 +0000

    add firebase config

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCYskikricNuUpjOol0kLrJBZk8f4xJQbc',
+  authDomain: 'kpm-to-do-app.firebaseapp.com',
+  projectId: 'kpm-to-do-app',
+  storageBucket: 'kpm-to-do-app.firebasestorage.app',
+  messagingSenderId: '852388772705',
+  appId: '1:852388772705:web:9aca044820f386e7e4a320',
+  measurementId: 'G-1VWWM7M4N4',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
