baf9af6633752f7be76906927cbd059c908cfab9
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-04 17:38:40 +0000

    add disabled property on load

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ButtonProps } from "../lib/types/types";
 
-export default function Button({id, children, type, onClick, onSubmit}: ButtonProps) {
+export default function Button({id, children, type, disabled = false, onClick, onSubmit}: ButtonProps) {
   return (
-    <button type={type} id={id} className='primary-button' onClick={onClick} onSubmit={onSubmit}>{children}</button>
+    <button disabled={disabled} type={type} id={id} className='primary-button' onClick={onClick} onSubmit={onSubmit}>{children}</button>
   )
 }
