c8e938e58a4170591a9d1638dae4a0369883bbe6
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-04 08:19:16 +0000

    add class to input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,6 @@ import { InputProps } from "../lib/types/types";
 
 export default function Input({id, name, placeholder, type, val, handleChange}: InputProps) {
   return (
-    <input id={id} name={name} placeholder={placeholder} type={type} value={val} onChange={(e)=>{handleChange(e.target.value, e.target.name)}}/>
+    <input id={id} className='input' name={name} placeholder={placeholder} type={type} value={val} onChange={(e)=>{handleChange(e.target.value, e.target.name)}}/>
   )
 }
