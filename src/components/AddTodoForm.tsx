5a217f2729b856ed3398e118084d796fd2387bf3
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-04 08:16:48 +0000

    add user to form

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { AddTodoFormProps } from '../lib/types/types';
 import Button from './Button';
 import Input from './Input';
+import { isValidDateFormat } from '../lib/utils/isValidDateFormat';
+import { UserContext } from '../contexts/User';
 
 export default function AddTodoForm({
+  activeTodos,
   addTodo,
-  setTodoList
+  setTodoList,
 }: AddTodoFormProps) {
+  const user = useContext(UserContext)
+  const canAddTodo = user?.userLevel === 'registered' || activeTodos < 5
+  
   const [formInputs, setFormInputs] = useState({
     text: '',
     priority: '🟢',
     finishBy: '',
   });
+  const [isWarningVisible, setIsWarningVisible] = useState(false)
 
   const handleChange = (value: string, name: string) => {
     setFormInputs((curr) => {
@@ -20,7 +27,64 @@ export default function AddTodoForm({
   };
 
   return (
-    <form className='add-todo' onSubmit={(e)=>{e.preventDefault(); addTodo({...formInputs, status:'incomplete', id: `${new Date()}`}, setTodoList)}}>
+    <form
+      className='add-todo'
+      onSubmit={(e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        const isValidDate = isValidDateFormat(formInputs.finishBy)
+
+        if (isValidDate && formInputs.text !== '' && canAddTodo) {
+          console.log('here');
+          
+          addTodo(
+            { ...formInputs, status: 'incomplete', id: `${new Date()}` },
+            setTodoList
+          );
+          setFormInputs({
+            text: '',
+            priority: '🟢',
+            finishBy: '',
+          });
+        } else {
+          if (!isValidDate) {
+            setTimeout(() => {
+              setFormInputs((curr) => {
+                const resetInputs = { ...curr };
+                resetInputs.finishBy = '';
+                return resetInputs;
+              });
+            }, 2000);
+            setFormInputs((curr) => {
+              const warningInputs = { ...curr };
+              warningInputs.finishBy = 'INCORRECT DATE FORMAT';
+              return warningInputs;
+            });
+          }
+          if (!formInputs.text) {
+            setTimeout(() => {
+              setFormInputs((curr) => {
+                const resetInputs = { ...curr };
+                resetInputs.text = '';
+                return resetInputs;
+              });
+            }, 2000);
+            setFormInputs((curr) => {
+              const warningInputs = { ...curr };
+              warningInputs.text = 'PLEASE INSERT TODO';
+              return warningInputs;
+            });
+          }
+          if(!canAddTodo) {
+            setTimeout(() => {
+              setIsWarningVisible(false)
+            }, 2000);
+            setIsWarningVisible(true)
+          }
+        }
+      }}
+    >
+      Add Todo
       <Input
         id='todo-text'
         name='text'
@@ -37,12 +101,29 @@ export default function AddTodoForm({
         type='text'
         val={formInputs.finishBy}
       />
-      <select name='priority' value={formInputs.priority} onChange={(e)=>{handleChange(e.target.value, 'priority')}}>
-        <option value='🔴'>🔴 High Priority</option>
-        <option value='🟡'>🟡 Mid Priority</option>
-        <option value='🟢'>🟢 Low Priority</option>
+      <select
+        id='dropdown'
+        className='input'
+        name='priority'
+        value={formInputs.priority}
+        onChange={(e) => {
+          handleChange(e.target.value, 'priority');
+        }}
+      >
+        <option id='dropdown1' value='🔴'>
+          🔴 High Priority
+        </option>
+        <option id='dropdown2' value='🟡'>
+          🟡 Mid Priority
+        </option>
+        <option id='dropdown3' value='🟢'>
+          🟢 Low Priority
+        </option>
       </select>
-      <Button type="submit" id="add-todo-button">ADD TODO</Button>
+      <Button type='submit' id='add-todo-button'>
+        ADD TODO
+      </Button>
+      {isWarningVisible && <p className='warning'>REGISTER OR SIGN IN TO ADD MORE TODOS</p>}
     </form>
   );
 }
