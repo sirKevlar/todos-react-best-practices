44538e8d8b0355b22ece0103ed6309bbaeaabf01
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-05 18:50:26 +0000

    add sign out

--- src/components/SignInOut.tsx
+++ src/components/SignInOut.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { auth } from "../firebase/firebase";
 import { SignInOutProps } from "../lib/types/types";
 import Button from "./Button";
+import { UserContext } from "../contexts/UserContext";
+import { GUEST_USER } from "../lib/constants/constants";
 
 
-export default function SignInOut({setIsRegisteredUser, setIsModalVisible}: SignInOutProps) {
+export default function SignInOut({setIsRegisteredUser, setIsModalVisible, setCurrentUser}: SignInOutProps) {
+    const user = useContext(UserContext)
 
     const handleSignIn = () => {
         setIsModalVisible(true)
@@ -12,11 +17,23 @@ export default function SignInOut({setIsRegisteredUser, setIsModalVisible}: Sign
         setIsModalVisible(true)
         setIsRegisteredUser(false)
     }
-
-  return (
-    <form id='sign-in-out'>
-        <Button id='sign-in-button' type='button' onClick={handleSignIn}>SIGN IN</Button>
-        <Button id='register-button' type='button' onClick={handleRegister}>REGISTER</Button>
+    const handleSignOut = () => {
+        auth.signOut()
+        setIsRegisteredUser(false)
+        setCurrentUser(GUEST_USER)
+    }
+    
+    return (
+      <form id='sign-in-out'>
+        {
+        user?.uid ? 
+        <Button id='sign-out-button' type='button' onClick={handleSignOut}>SIGN OUT</Button> 
+        : 
+        <>
+          <Button id='sign-in-button' type='button' onClick={handleSignIn}>SIGN IN</Button>
+          <Button id='register-button' type='button' onClick={handleRegister}>REGISTER</Button>
+        </>
+        }
     </form>
   )
 }
