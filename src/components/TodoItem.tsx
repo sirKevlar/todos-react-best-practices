0cc6ffda292b348ba4ad0d7f92b1f33a2fff14ba
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-05 23:05:06 +0000

    formatting

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,6 +4,6 @@ export default function TodoItem({children, id, text, priority, finishBy, status
     const finishDate = `${finishBy[4]}${finishBy[5]}/${finishBy[2]}${finishBy[3]}/20${finishBy[0]}${finishBy[1]}`
 
     return (
-    <div id={id} className="todo flex justify-between"><p id={`priority${id}`}>{priority}</p><p className={`todo-text ${status}`} onClick={()=>{changeTodoStatus(id, setTodoList)}}>{text} by {finishDate} </p>{children}</div>
+    <div id={id} className="todo flex justify-between"><p id={`priority${id}`}>{priority}</p><p className={`todo-text ${status}`} onClick={()=>{changeTodoStatus(id, setTodoList)}}> {text} by {finishDate} </p>{children}</div>
   )
 }
