6f85a1c5dba7734586fdddf4743adccd41b6d379
Amanda-WJ <126867885+Amanda-WJ@users.noreply.github.com>
2024-12-07 10:33:07 +0000

    add button background div

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,6 @@ import { ButtonProps } from "../lib/types/types";
 
 export default function Button({id, children, type, disabled = false, onClick, onSubmit}: ButtonProps) {
   return (
-    <button disabled={disabled} type={type} id={id} className='primary-button' onClick={onClick} onSubmit={onSubmit}>{children}</button>
+    <div className="button-background"><button disabled={disabled} type={type} id={id} className='primary-button' onClick={onClick} onSubmit={onSubmit}>{children}</button></div>
   )
 }
